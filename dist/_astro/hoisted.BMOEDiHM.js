fc87666468bd67b1662c093b7781067839f43842
Khaled Saifullah <ksnirob@gmail.com>
2026-07-03 20:17:11 +0600

    form and chat panel issue fixed

--- dist/_astro/hoisted.BMOEDiHM.js
+++
@@ -1,1 +0,0 @@
-import"./Footer.astro_astro_type_script_index_0_lang.BDWdvS5k.js";import"https://link.msgsndr.com/js/form_embed.js";
---
+++ dist/_astro/hoisted.C1fmRKCp.js
@@ -0,0 +1,1 @@
+import"./CookieConsent.astro_astro_type_script_index_0_lang.J6_f2tfS.js";import"https://link.msgsndr.com/js/form_embed.js";
