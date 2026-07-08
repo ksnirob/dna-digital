ac30d4fd492655066a90fda49e34208df1aa5d87
Khaled Saifullah <ksnirob@gmail.com>
2026-07-08 16:44:39 +0600

    contact details updated

--- dist/_astro/hoisted.C1fmRKCp.js
+++
@@ -1,1 +0,0 @@
-import"./CookieConsent.astro_astro_type_script_index_0_lang.J6_f2tfS.js";import"https://link.msgsndr.com/js/form_embed.js";
---
+++ dist/_astro/hoisted.DnpulEjs.js
@@ -0,0 +1,1 @@
+import"./CookieConsent.astro_astro_type_script_index_0_lang.gjpMywI7.js";import"https://link.msgsndr.com/js/form_embed.js";
